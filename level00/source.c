c4cf3c0d41b5ae29ff5cdec6231c60bfecf197d9
krambono <krambono@student.42lyon.fr>
2020-12-20 16:49:54 +0100

    level00

--- level00/source.c
+++ level00/source.c
@@ -5,4 +5,19 @@
 
 int main(int argc, const char **argv)
 {
+	int a; // esp + 0x1c
+
+	puts("***********************************");
+	puts("* \t     -Level00 -\t\t  *");
+	puts("***********************************");
+	printf("Password:");
+	scanf("%d", &a);
+	if (a != 5276)
+	{
+		puts("\nInvalid Password!");
+		return 1;
+	}
+	puts("\nAuthenticated!");
+	system("/bin/sh");
+	return 0;
 }
